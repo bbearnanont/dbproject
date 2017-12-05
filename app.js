80a668de833d0b38075bbb2b4ce26e44067a59d0
lvlascot <smo_like_u@hotmail.co.th>
2017-12-05 16:40:06 +0700

    Commit jak game

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.set('views', path.join(__dirname, '/App/views'));
 app.use(bodyParser());
 app.engine('html', ejs.renderFile);  
 app.listen(port);
-app.use(express.static('public'));
+app.use(express.static('public'))
 
