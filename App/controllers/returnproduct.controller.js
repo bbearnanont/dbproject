b9b10e19449f92b0f093704839a50f6858a28ad4
Unknown <chain130740@hotmail.co.th>
2017-12-08 10:58:14 +0700

    Fianalll

--- App/controllers/returnproduct.controller.js
+++ App/controllers/returnproduct.controller.js
@@ -35,14 +35,12 @@ exports.showItem = function (req, res){
                 return;
             }
             else    
-<<<<<<< HEAD
-                
+
                 {
                     res.render('returnproduct.html',{item:result,purchase_order_list:result2, userlevel:userlevel});
                 }
-=======
                 res.render('returnproduct.html',{item:result,purchase_order_list:result2, userlevel:userlevel});
->>>>>>> master
+
                     
                 }
             );
